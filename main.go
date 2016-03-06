5a0a41de4cfbe3acd0686ded1145ea9a045d5014
侯小龙 <houxiaolong@yunyao-inc.com>
2016-03-06 15:34:35 +0800

    qiniu_download

--- main.go
+++ main.go
@@ -2,25 +2,29 @@ package main
 
 import (
 	"cloudStore/store"
-	"fmt"
 	"os"
 )
 
 func main() {
 	var s store.Store
-	accessKey := ""
-	secretKey := ""
+	accessKey := "O3TdDpbhK1iLGT7lEIw8SD7k41D1I2Z_YItPYizz"
+	secretKey := "sjI9C6b7EqO9NEJb9g4ow0z1dozkhqL0r5-K2kUx"
+	domain := "7xrkxc.com1.z0.glb.clouddn.com"
 	s = store.NewQiniu(accessKey, secretKey, 0)
 	// s.UploadFile("test", "/Users/mac/cutting/00.jpg")
-	file, err := os.Open("/Users/mac/cutting/00.jpg")
+	file, err := os.Open("/Users/mac/Downloads/test.jpg")
 	defer file.Close()
 	if err != nil {
 		panic(err)
 	}
 	info, _ := file.Stat()
-	fmt.Println(info.Size())
-	err = s.UploadData("test", file, info.Size())
+	data, err := s.UploadData("test", file, info.Size())
 	if err != nil {
 		panic(err)
 	}
+	rc, err := s.Download(domain, data)
+	if err != nil {
+		panic(err)
+	}
+	defer rc.Close()
 }
--- store/store.go
+++ store/store.go
@@ -1,17 +1,17 @@
 package store
 
 import (
-	"fmt"
 	"io"
+	"net/http"
 
 	"golang.org/x/net/context"
 	"qiniupkg.com/api.v7/kodo"
 )
 
 type Store interface {
-	UploadFile(bucketName, localFile string) error
-	UploadData(bucketName string, reader io.Reader, sizeOfFile int64) error
-	Download()
+	UploadFile(bucketName, localFile string) (string, error)
+	UploadData(bucketName string, reader io.Reader, sizeOfFile int64) (string, error)
+	Download(domain, key string) (io.ReadCloser, error)
 }
 
 type Qiniu struct {
@@ -19,29 +19,36 @@ type Qiniu struct {
 	context context.Context
 }
 
-func NewQiniu(accessKey, secretKey string, zone int) Qiniu {
+func NewQiniu(accessKey, secretKey string, zone int) *Qiniu {
 	kodo.SetMac(accessKey, secretKey)
 	cxt := context.Background()
 	cli := kodo.New(zone, nil)
-	return Qiniu{cli, cxt}
+	return &Qiniu{cli, cxt}
 }
 
-func (qn Qiniu) UploadFile(bucketName, localFile string) error {
+func (qn *Qiniu) UploadFile(bucketName, localFile string) (string, error) {
 	bucket := qn.client.Bucket(bucketName)
 	var ret kodo.PutRet
 	err := bucket.PutFileWithoutKey(qn.context, &ret, localFile, nil)
-	fmt.Println(ret)
-	return err
+	if err != nil {
+		return "", err
+	}
+	return ret.Key, nil
 }
 
-func (qn Qiniu) UploadData(bucketName string, reader io.Reader, sizeOfFile int64) error {
+func (qn *Qiniu) UploadData(bucketName string, reader io.Reader, sizeOfFile int64) (string, error) {
 	bucket := qn.client.Bucket(bucketName)
 	var ret kodo.PutRet
 	err := bucket.PutWithoutKey(qn.context, &ret, reader, sizeOfFile, nil)
-	fmt.Println(ret)
-	return err
+	if err != nil {
+		return "", err
+	}
+	return ret.Key, nil
 }
 
-func (qn Qiniu) Download() {
-
+func (qn *Qiniu) Download(domain, key string) (io.ReadCloser, error) {
+	baseUrl := kodo.MakeBaseUrl(domain, key)
+	privateUrl := qn.client.MakePrivateUrl(baseUrl, nil)
+	resp, err := http.Get(privateUrl)
+	return resp.Body, err
 }
